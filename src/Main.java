f9995c59cf58a221786ea8f520e037169bdf2a02
Flunny <iceking7777@mail.ru>
2019-12-05 17:00:02 +0400

    Домашнее задание с числами Фибоначи и Факториалом

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int piglet = 0;
+        int ham = 1;
+        int pork;
+        for (int i = 0; i < 10; i++) {
+            pork = piglet + ham;
+            piglet = ham;
+            ham = pork;
+            System.out.println(pork);
+        }
+        int Joooo = 1;
+        for (int i=1; i<7; i++) {
+            Joooo= Joooo * i;}
+        System.out.println(Joooo);
     }
 }
